681eeef660dbfdcc81756ea2221a7f6fb286853c
Leoodaviid <david_ax7@hotmail.com>
2023-01-04 20:45:45 -0300

    padronizando tela de login com o hook personalizado useForm

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,17 +1,19 @@
-import React from "react";
-
-import { LogoStyle, Input, Hr, MiniImg } from "./Styles";
-import { ButtonStyle } from "../../components/Button/style";
-
+import { LogoStyle, Hr, MiniImg } from "./Styles";
 import logo from "../../assets/logo.png";
 import appleLogo from "../../assets/appleLogo.png";
 import googleLogo from "../../assets/googleLogo.png";
 import facebookLogo from "../../assets/facebookLogo.png";
 import { Navigate, Link } from "react-router-dom";
 import { FormEvent } from "react";
+import useForm from "../../components/Hooks/useForm";
+import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
 // import Cadastro from '../Cadastro/cadastro';
 
 export function Login() {
+  const username = useForm("nome");
+  const password = useForm("senha");
+
   function handleSubmit(event: FormEvent) {
     event.preventDefault();
     <Navigate to="/cadastro" />;
@@ -21,13 +23,21 @@ export function Login() {
     <div>
       <LogoStyle src={logo} alt="logo" />
       <form onSubmit={handleSubmit}>
-        <Input type="text" placeholder="Login" />
-        <Input type="password" placeholder="Senha" />
-        <ButtonStyle>
-          <Link to="/cadastro" className="btn text-black px-5 fw-bold">
-            Entrar
-          </Link>
-        </ButtonStyle>
+        <Input
+          label="Nome"
+          type="text"
+          placeholder="Nome"
+          name="username"
+          {...username}
+        />
+        <Input
+          label="Senha"
+          type="password"
+          placeholder="Senha"
+          name="password"
+          {...password}
+        />
+        <Button>Entrar</Button>
       </form>
 
       <div className="d-flex justify-content-around mt-4">
