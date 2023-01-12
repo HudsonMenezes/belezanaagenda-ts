feaedd06e649cdd3560cc207bb0f5a4fea257a38
Leoodaviid <david_ax7@hotmail.com>
2023-01-12 12:32:39 -0300

    adicionando contexto para uso em toda aplicaçao

--- src/Router.tsx
+++ src/Router.tsx
@@ -11,23 +11,26 @@ import PaginaInicial from "./pages/PaginaInicial/PaginaInicial";
 import Agendamento from "./pages/Agendamento/Agendamento";
 import AgendamentoConcluido from "./pages/AgendamentoConcluido/AgendamentoConcluido";
 import Perfil from "./pages/Perfil/Perfil";
+import { UserStorage } from "./UserContext";
 
 export default function Routes() {
   return (
     <div className="App">
       <BrowserRouter>
-        <WrapperRoutes>
-          <Route path="/" element={<Login />} />
-          <Route path="/cadastro" element={<Cadastro />} />
-          <Route path="/cadastroconcluido" element={<CadastroConcluido />} />
-          <Route path="/paginainicial" element={<PaginaInicial />} />
-          <Route path="/agendamento" element={<Agendamento />} />
-          <Route
-            path="/agendamentoconcluido"
-            element={<AgendamentoConcluido />}
-          />
-          <Route path="/perfil" element={<Perfil />} />
-        </WrapperRoutes>
+        <UserStorage>
+          <WrapperRoutes>
+            <Route path="/" element={<Login />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/cadastroconcluido" element={<CadastroConcluido />} />
+            <Route path="/paginainicial" element={<PaginaInicial />} />
+            <Route path="/agendamento" element={<Agendamento />} />
+            <Route
+              path="/agendamentoconcluido"
+              element={<AgendamentoConcluido />}
+            />
+            <Route path="/perfil" element={<Perfil />} />
+          </WrapperRoutes>
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
