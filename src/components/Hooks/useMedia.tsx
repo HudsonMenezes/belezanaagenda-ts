9cebbead81de6d33f52170186529bb322b3b06f1
Leoodaviid <david_ax7@hotmail.com>
2023-01-18 02:45:52 -0300

    hook personalizado para controle do resize de telas

---
+++ src/components/Hooks/useMedia.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+const useMedia = (media: string) => {
+  const [match, setMatch] = useState<boolean>(false);
+
+  useEffect(() => {
+    function changeMatch() {
+      const { matches } = window.matchMedia(media);
+      setMatch(matches);
+    }
+    changeMatch();
+    window.addEventListener("resize", changeMatch);
+    return () => {
+      window.removeEventListener("resize", changeMatch);
+    };
+  }, [media]);
+  return match;
+};
+
+export default useMedia;
