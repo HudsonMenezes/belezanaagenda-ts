be6e28fbfbec3a76121f0545bea521de0dccd647
Leoodaviid <david_ax7@hotmail.com>
2023-01-19 10:20:59 -0300

    adicionando novo contexto para agenda do usuario cliente

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,7 @@ import {
   Routes as WrapperRoutes,
   Route,
 } from "react-router-dom";
-import { UserStorage } from "./UserContext";
+import { UserStorage } from "./components/Contexts/UserContext";
 import Agenda from "./pages/Agenda/Agenda";
 import AgendaData from "./pages/AgendaData/AgendaData";
 import AgendaHorario from "./pages/AgendaHorario/AgendaHorario";
@@ -14,24 +14,27 @@ import Perfil from "./pages/Perfil/Perfil";
 import AgendaSucesso from "./pages/AgendaSucesso/AgendaSucesso";
 import AgendaProfissional from "./pages/AgendaProfissional/AgendaProfissional";
 import ProtectedRouter from "./components/Helper/protectedRouter";
+import { UserAgendaStorage } from "./components/Contexts/UserAgenda";
 
 export default function Routes() {
   return (
     <div className="App">
       <BrowserRouter>
         <UserStorage>
-          <WrapperRoutes>
-            <Route path="/" element={<Login />} />
-            <Route path="/cadastro" element={<Cadastro />} />
-            <Route element={<ProtectedRouter />}>
-              <Route path="/agenda" element={<Agenda />} />
-              <Route path="/data" element={<AgendaData />} />
-              <Route path="/horario" element={<AgendaHorario />} />
-              <Route path="/profissional" element={<AgendaProfissional />} />
-              <Route path="/sucesso" element={<AgendaSucesso />} />
-              <Route path="/perfil" element={<Perfil />} />
-            </Route>
-          </WrapperRoutes>
+          <UserAgendaStorage>
+            <WrapperRoutes>
+              <Route path="/" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route element={<ProtectedRouter />}>
+                <Route path="/agenda" element={<Agenda />} />
+                <Route path="/data" element={<AgendaData />} />
+                <Route path="/horario" element={<AgendaHorario />} />
+                <Route path="/profissional" element={<AgendaProfissional />} />
+                <Route path="/sucesso" element={<AgendaSucesso />} />
+                <Route path="/perfil" element={<Perfil />} />
+              </Route>
+            </WrapperRoutes>
+          </UserAgendaStorage>
         </UserStorage>
       </BrowserRouter>
     </div>
