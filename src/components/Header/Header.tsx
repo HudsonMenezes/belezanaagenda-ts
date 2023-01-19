da3a93aff13dd7cb0c8b4cf6156647e8493c4857
Hudson Menezes <hudson.bm23@gmail.com>
2023-01-19 17:43:51 -0300

    link da logo consertado

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,12 +2,15 @@
 import { HeaderStyle } from "./Styles";
 import Logo from "../../assets/logo3.png";
 import Sidebar from "../Sidebar/Sidebar";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <>
       <HeaderStyle className="headerStyle d-flex justify-content-between align-items-center">
-        <img src={Logo} alt="Logo" className="logo" />
+        <Link to="/agenda">
+          <img src={Logo} alt="Logo" className="logo" />
+        </Link>
         <Sidebar />
       </HeaderStyle>
     </>
