bcbc30ed499d52284f827b06990d4a6e4bde4bdc
Leoodaviid <david_ax7@hotmail.com>
2023-01-19 21:13:31 -0300

    Ajustes userLogin

--- src/components/Contexts/UserContext.tsx
+++ src/components/Contexts/UserContext.tsx
@@ -60,11 +60,10 @@ export const UserStorage = ({ children }: { children: ReactNode }) => {
       setError(null);
       setLoading(true);
       const response = await loginCliente({ email, senha });
-      if (response.status !== 201)
+      if (response.status !== 200)
         throw new Error(`Error: ${response.statusText}`);
       const token = response.data.token;
       const id = response.data.cliente._id;
-      // await getUser(id);
       setToken("token", token);
       setId("id", id);
       navigate("/agenda");
