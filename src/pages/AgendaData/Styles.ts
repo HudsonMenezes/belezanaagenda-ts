63c922771ccd1f50aeb33b085a8f673b2349a81d
Leoodaviid <david_ax7@hotmail.com>
2023-01-20 03:14:52 -0300

    Ajustes de estilos

--- src/pages/AgendaData/Styles.ts
+++ src/pages/AgendaData/Styles.ts
@@ -12,4 +12,53 @@ export const TextoPasso = styled.p`
     color: #742092;
     font-weight: 700;
   }
+  @media (max-width: 40rem) {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding-left: 1rem;
+  }
+`;
+export const SectionStepOne = styled.section`
+  width: 100%;
+  height: auto;
+  box-sizing: border-box;
+  background-color: #fff;
+
+  .container-grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    min-height: 865px;
+  }
+
+  .textStep {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #ffc973;
+    min-height: 90%;
+  }
+
+  @media (max-width: 40rem) {
+    .container-grid {
+      grid-template-columns: 1fr;
+    }
+    .textStep {
+      background-color: transparent;
+      position: relative;
+      bottom: -90%;
+      right: 21%;
+    }
+    .datePicker {
+      height: auto;
+    }
+    .step {
+      position: relative;
+      top: -11%;
+    }
+    .backButton {
+      position: relative;
+      top: -230px;
+    }
+  }
 `;
